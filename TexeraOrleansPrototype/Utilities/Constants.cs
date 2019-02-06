a2f2d6c722a5973c50403a17157e45948fc7046b
Avinash Kumar <avinash0161@yahoo.com>
2019-02-06 14:12:06 -0800

    added retry number

--- TexeraOrleansPrototype/Utilities/Constants.cs
+++ TexeraOrleansPrototype/Utilities/Constants.cs
@@ -14,6 +14,7 @@ namespace TexeraUtilities
         public const string ControllerAssemblyPathPrefix = "Engine.Controller";
         public const int batchSize = 1000;
         public const int num_scan = 10;
+        public const int max_retries = 60;
         public const bool conditions_on = false;
         public const bool ordered_on = true;
         public const string dataset = "small";
