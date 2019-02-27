3e660c6b0a964b1eff133f3a37178fd14a80817e
Shengquan Ni <Hiseen@users.noreply.github.com>
2019-02-26 21:06:34 -0800

    revert numbers

--- TexeraOrleansPrototype/Utilities/Constants.cs
+++ TexeraOrleansPrototype/Utilities/Constants.cs
@@ -12,8 +12,8 @@ namespace TexeraUtilities
     {
         public const string OperatorAssemblyPathPrefix = "Engine.OperatorImplementation.Operators";
         public const string ControllerAssemblyPathPrefix = "Engine.Controller";
-        public const int batchSize = 100;
-        public const int num_scan = 2;
+        public const int batchSize = 1000;
+        public const int num_scan = 10;
         public const int max_retries = 60;
         public const bool conditions_on = false;
         public const bool ordered_on = true;
