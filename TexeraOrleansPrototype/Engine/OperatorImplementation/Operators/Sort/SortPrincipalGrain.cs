0c5022e4e0761835e0bcddec83ff5306475d8e55
Shengquan Ni <Hiseen@users.noreply.github.com>
2019-05-15 16:42:44 -0700

    bug fix

--- TexeraOrleansPrototype/Engine/OperatorImplementation/Operators/Sort/SortPrincipalGrain.cs
+++ TexeraOrleansPrototype/Engine/OperatorImplementation/Operators/Sort/SortPrincipalGrain.cs
@@ -12,11 +12,9 @@ namespace Engine.OperatorImplementation.Operators
     public class SortPrinicipalGrain<T> : PrincipalGrain, ISortPrincipalGrain<T> where T:IComparable<T>
     {
         public override int DefaultNumGrainsInOneLayer { get { return 1; } }
-        public override async Task<IWorkerGrain> GetOperatorGrain(string extension)
+        public override IWorkerGrain GetOperatorGrain(string extension)
         {
-            var grain=this.GrainFactory.GetGrain<ISortOperatorGrain<T>>(this.GetPrimaryKey(), extension);
-            await grain.Init(grain,predicate,self);
-            return grain;
+            return this.GrainFactory.GetGrain<ISortOperatorGrain<T>>(this.GetPrimaryKey(), extension);
         }
     }
 }
