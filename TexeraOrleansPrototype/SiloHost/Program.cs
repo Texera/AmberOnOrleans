e736374132fd75eb812fa5c62d58d400e63b3dea
Shengquan Ni <Hiseen@users.noreply.github.com>
2019-05-19 17:10:33 -0700

    update

--- TexeraOrleansPrototype/SiloHost/Program.cs
+++ TexeraOrleansPrototype/SiloHost/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Orleans;
 using Orleans.Configuration;
@@ -13,12 +14,17 @@ namespace SiloHost
     public class Program
     {
         static ISiloHost host;
+        static ManualResetEvent _quitEvent = new ManualResetEvent(false);
         public static void Main(string[] args)
         {
+            Console.CancelKeyPress += (sender, eArgs) => {
+                _quitEvent.Set();
+                eArgs.Cancel = true;
+            };
             Console.WriteLine("Ready to build connection...");
             host=SiloWrapper.Instance.host;
             Console.WriteLine("Silo Started!");
-            Console.ReadLine();
+            _quitEvent.WaitOne();
         }
     }
 } 
