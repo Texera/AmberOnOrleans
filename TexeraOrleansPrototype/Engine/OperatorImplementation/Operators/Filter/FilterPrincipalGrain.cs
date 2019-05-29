b78b84cd0dc290119ebbb1d944d1cf2899fe4faf
Hiseen <hiseen@github.com>
2019-05-28 18:54:21 -0700

    update

--- TexeraOrleansPrototype/Engine/OperatorImplementation/Operators/Filter/FilterPrincipalGrain.cs
+++ TexeraOrleansPrototype/Engine/OperatorImplementation/Operators/Filter/FilterPrincipalGrain.cs
@@ -12,7 +12,7 @@ namespace Engine.OperatorImplementation.Operators
     public class FilterPrinicipalGrain<T> : PrincipalGrain, IFilterPrincipalGrain<T> where T:IComparable<T>
     {
 
-        public override int DefaultNumGrainsInOneLayer { get { return 20; } } 
+        public override int DefaultNumGrainsInOneLayer { get { return 30; } } 
 
         public override async Task Init(Controller.IControllerGrain controllerGrain, Guid workflowID, Operator currentOperator)
         {
