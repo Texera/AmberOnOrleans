36e13252e7f2519701b2a28beb2a06de725e0800
Shengquan Ni <Hiseen@users.noreply.github.com>
2019-06-04 23:54:51 -0700

    code change

--- TexeraOrleansPrototype/Engine/OperatorImplementation/SendingSemantics/SingleQueueSendStrategy.cs
+++ TexeraOrleansPrototype/Engine/OperatorImplementation/SendingSemantics/SingleQueueSendStrategy.cs
@@ -49,19 +49,19 @@ namespace Engine.OperatorImplementation.SendingSemantics
 
         public abstract Task SendEndMessages(IGrain senderIdentifier);
 
-        protected Task SendMessageTo(IWorkerGrain nextGrain,Immutable<PayloadMessage> message,int retryCount)
+        protected async Task SendMessageTo(IWorkerGrain nextGrain,Immutable<PayloadMessage> message,int retryCount)
         {
-            nextGrain.ReceivePayloadMessage(message).ContinueWith((t)=>{
+            await nextGrain.ReceivePayloadMessage(message).ContinueWith(async (t)=>{
                 if(Utils.IsTaskTimedOutAndStillNeedRetry(t,retryCount))
                 {
                     string sender,receiver;
                     sender=Utils.GetReadableName(message.Value.SenderIdentifer);
                     receiver=Utils.GetReadableName(nextGrain);
                     Console.WriteLine(sender+" re-send message with sequence num: "+message.Value.SequenceNumber +" to "+receiver+" with retry count "+retryCount);
-                    SendMessageTo(nextGrain,message, retryCount + 1);
+                    await SendMessageTo(nextGrain,message, retryCount + 1);
                 }
             });
-            return Task.CompletedTask;
+            //return Task.CompletedTask;
             //.ContinueWith(async (t)=>
             // {
             // if(Utils.IsTaskTimedOutAndStillNeedRetry(t,retryCount))
