1cbd360b47aa2aa7dfd2319542a894ea1c056b6f
Shengquan Ni <Hiseen@users.noreply.github.com>
2019-06-06 23:37:47 -0700

    debug output

--- TexeraOrleansPrototype/Engine/OperatorImplementation/SendingSemantics/FlowControlUnit.cs
+++ TexeraOrleansPrototype/Engine/OperatorImplementation/SendingSemantics/FlowControlUnit.cs
@@ -25,6 +25,7 @@ public class FlowControlUnit
 
     public void Send(Immutable<PayloadMessage> message) 
     {
+        Console.WriteLine(Utils.GetReadableName(message.Value.SenderIdentifer)+" -> "+Utils.GetReadableName(receiver));
         if (message.Value.SequenceNumber - lastAckSeqNum > windowSize) 
         {
             toBeSentBuffer.Enqueue(message);
@@ -46,13 +47,13 @@ public class FlowControlUnit
         {
             if (Utils.IsTaskTimedOutAndStillNeedRetry(t,retryCount))
             {
-                Console.WriteLine(Utils.GetReadableName(message.Value.SenderIdentifer)+" -> "+Utils.GetReadableName(receiver)+" resend "+message.Value.SequenceNumber);
+                //Console.WriteLine(Utils.GetReadableName(message.Value.SenderIdentifer)+" -> "+Utils.GetReadableName(receiver)+" resend "+message.Value.SequenceNumber);
                 SendInternal(message,retryCount+1);
             } 
             else
             {
                 windowSize = t.Result;
-                Console.WriteLine(Utils.GetReadableName(message.Value.SenderIdentifer)+" -> "+Utils.GetReadableName(receiver)+" window size = "+windowSize);
+                //Console.WriteLine(Utils.GetReadableName(message.Value.SenderIdentifer)+" -> "+Utils.GetReadableName(receiver)+" window size = "+windowSize);
                 // action for successful ack
                 if (message.Value.SequenceNumber <= lastAckSeqNum) 
                 {
