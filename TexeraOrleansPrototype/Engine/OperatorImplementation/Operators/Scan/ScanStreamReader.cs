ae41e06e253b648d9b04c018b1d006418b77f4b1
Shengquan Ni <Hiseen@users.noreply.github.com>
2019-06-08 22:52:25 -0700

    update scan buffer

--- TexeraOrleansPrototype/Engine/OperatorImplementation/Operators/Scan/ScanStreamReader.cs
+++ TexeraOrleansPrototype/Engine/OperatorImplementation/Operators/Scan/ScanStreamReader.cs
@@ -8,7 +8,7 @@ using Engine.OperatorImplementation.Common;
 
 class ScanStreamReader
 {
-    public const int buffer_size = 4096;
+    public const int buffer_size = 1024*64;
     private enum FileType{unknown,csv,pdf,txt,tbl};
     private FileType file_type;
     private string file_path;
