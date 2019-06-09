9ecf9febe8535293d239d5955e2ee31b34f09fc4
Shengquan Ni <Hiseen@users.noreply.github.com>
2019-06-09 16:55:56 -0700

    change window size to 4

--- TexeraOrleansPrototype/Engine/OperatorImplementation/SendingSemantics/FlowControlUnit.cs
+++ TexeraOrleansPrototype/Engine/OperatorImplementation/SendingSemantics/FlowControlUnit.cs
@@ -13,7 +13,7 @@ namespace Engine.OperatorImplementation.SendingSemantics
 {
     public class FlowControlUnit: SendingUnit
     {
-        int windowSize = 1;
+        int windowSize = 4;
         HashSet<ulong> messagesOnTheWay=new HashSet<ulong>();
         Queue<Immutable<PayloadMessage>> toBeSentBuffer=new Queue<Immutable<PayloadMessage>>();
 
