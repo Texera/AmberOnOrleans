ea23be4659386f98342aa65a6a7922bef7438f83
Shengquan Ni <Hiseen@users.noreply.github.com>
2019-06-08 23:52:54 -0700

    speed uppp

--- TexeraOrleansPrototype/Engine/OperatorImplementation/SendingSemantics/FlowControlUnit.cs
+++ TexeraOrleansPrototype/Engine/OperatorImplementation/SendingSemantics/FlowControlUnit.cs
@@ -13,7 +13,7 @@ namespace Engine.OperatorImplementation.SendingSemantics
 {
     public class FlowControlUnit: SendingUnit
     {
-        int windowSize = 128;
+        int windowSize = 32;
         HashSet<ulong> messagesOnTheWay=new HashSet<ulong>();
         Queue<Immutable<PayloadMessage>> toBeSentBuffer=new Queue<Immutable<PayloadMessage>>();
 
