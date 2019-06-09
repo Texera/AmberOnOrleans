2ec0ba6844de7104ace5d597e1874af0096e7e8b
Shengquan Ni <Hiseen@users.noreply.github.com>
2019-06-09 00:17:03 -0700

    update code

--- TexeraOrleansPrototype/Engine/OperatorImplementation/SendingSemantics/FlowControlUnit.cs
+++ TexeraOrleansPrototype/Engine/OperatorImplementation/SendingSemantics/FlowControlUnit.cs
@@ -13,7 +13,7 @@ namespace Engine.OperatorImplementation.SendingSemantics
 {
     public class FlowControlUnit: SendingUnit
     {
-        int windowSize = 72;
+        int windowSize = 32;
         HashSet<ulong> messagesOnTheWay=new HashSet<ulong>();
         Queue<Immutable<PayloadMessage>> toBeSentBuffer=new Queue<Immutable<PayloadMessage>>();
 
