6cefb90bc197757b271314e125a435f25a82ea1f
Shengquan Ni <Hiseen@users.noreply.github.com>
2019-06-12 13:08:21 -0700

    limit window to 4 again

--- TexeraOrleansPrototype/Engine/OperatorImplementation/SendingSemantics/FlowControlUnit.cs
+++ TexeraOrleansPrototype/Engine/OperatorImplementation/SendingSemantics/FlowControlUnit.cs
@@ -14,7 +14,7 @@ namespace Engine.OperatorImplementation.SendingSemantics
     public class FlowControlUnit: SendingUnit
     {
         static readonly TimeSpan okTime=new TimeSpan(0,0,0,5); 
-        static readonly int WindowSizeLimit=16;
+        static readonly int WindowSizeLimit=4;
         int ssthreshold = 4;
         int windowSize = 2;
         bool isPaused=false;
