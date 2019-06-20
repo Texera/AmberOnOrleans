23fac0a87ea1134803a1b4630cd3a35235dcd8a4
Shengquan Ni <Hiseen@users.noreply.github.com>
2019-06-20 00:34:15 -0700

    change cluster setting

--- TexeraOrleansPrototype/Utilities/Constants.cs
+++ TexeraOrleansPrototype/Utilities/Constants.cs
@@ -10,10 +10,10 @@ namespace TexeraUtilities
 {
     static public class Constants
     {
-        public const string ClientIPAddress="128.195.52.129";
+        public const string ClientIPAddress="10.138.15.198";
         public const int MaxRetries = 60;
         public const int BatchSize = 400;
-        public const int DefaultNumGrainsInOneLayer=2;
+        public const int DefaultNumGrainsInOneLayer=10;
         public static string ConnectionString = "server="+ClientIPAddress+";uid=orleans-backend;pwd=orleans-0519-2019;database=orleans;SslMode=none";
     }
 }
