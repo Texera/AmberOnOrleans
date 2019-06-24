60ee5fd2a6ca1063f7ca22af3abe064f758b2b0b
Shengquan Ni <Hiseen@users.noreply.github.com>
2019-06-24 11:43:54 -0700

    set it to 8

--- TexeraOrleansPrototype/Utilities/Constants.cs
+++ TexeraOrleansPrototype/Utilities/Constants.cs
@@ -13,7 +13,7 @@ namespace TexeraUtilities
         public const string ClientIPAddress="10.138.15.198";
         public const int MaxRetries = 60;
         public const int BatchSize = 400;
-        public const int DefaultNumGrainsInOneLayer=10;
+        public const int DefaultNumGrainsInOneLayer=8;
         public static string ConnectionString = "server="+ClientIPAddress+";uid=orleans-backend;pwd=orleans-0519-2019;database=orleans;SslMode=none";
     }
 }
