5e9cc7673dec96d95c398c06451aed4922b9158a
Shengquan Ni <Hiseen@users.noreply.github.com>
2019-06-25 14:56:28 -0700

    change setting

--- TexeraOrleansPrototype/Engine/OperatorImplementation/Operators/Scan/ScanPrincipalGrain.cs
+++ TexeraOrleansPrototype/Engine/OperatorImplementation/Operators/Scan/ScanPrincipalGrain.cs
@@ -12,7 +12,7 @@ namespace Engine.OperatorImplementation.Operators
 {
     public class ScanPrinicipalGrain : PrincipalGrain, IScanPrincipalGrain
     {
-        public override int DefaultNumGrainsInOneLayer { get { return 15; } }
+        public override int DefaultNumGrainsInOneLayer { get { return 20; } }
 
         public override IWorkerGrain GetOperatorGrain(string extension)
         {
