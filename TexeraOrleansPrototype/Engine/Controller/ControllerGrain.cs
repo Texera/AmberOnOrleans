fe33205c90990dddaa91e5e6f3ad2947f4a824d3
Shengquan Ni <Hiseen@users.noreply.github.com>
2019-06-28 22:20:13 -0700

    ouptut

--- TexeraOrleansPrototype/Engine/Controller/ControllerGrain.cs
+++ TexeraOrleansPrototype/Engine/Controller/ControllerGrain.cs
@@ -55,7 +55,7 @@ namespace Engine.Controller
         public Task OnTaskDidPaused()
         {    
             currentPausedPrincipals++;
-            //Console.WriteLine(targetPausedPrincipals);
+            Console.WriteLine(targetPausedPrincipals);
             if(currentPausedPrincipals==targetPausedPrincipals)
             {
                 TimeSpan duration=DateTime.UtcNow-pauseStart;
