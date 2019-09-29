840e4eb997ad2fb57b927237297eeb8eecff39f5
Shengquan Ni <Hiseen@github.noreply.com>
2019-09-29 12:54:12 -0700

    Update Constants.cs

--- TexeraOrleansPrototype/Utilities/Constants.cs
+++ TexeraOrleansPrototype/Utilities/Constants.cs
@@ -14,7 +14,7 @@ namespace TexeraUtilities
         public static string WebHDFSEntry = "http://34.68.182.10:9870/webhdfs/v1/";
         public static int MaxRetries = 60;
         public static int BatchSize = 400;
-        public static int DefaultNumGrainsInOneLayer=2;
+        public static int DefaultNumGrainsInOneLayer=10;
         public static string ConnectionString 
         {
             get
