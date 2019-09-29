788ecf4f10ee9a369f0fcdaf12e07dde865a9296
Shengquan Ni <Hiseen@github.noreply.com>
2019-09-28 22:52:41 -0700

    migrating changes

--- TexeraOrleansPrototype/webapi-project/Startup.cs
+++ TexeraOrleansPrototype/webapi-project/Startup.cs
@@ -24,8 +24,7 @@ namespace webapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(option => option.EnableEndpointRouting = false)
-                .AddNewtonsoftJson();
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
