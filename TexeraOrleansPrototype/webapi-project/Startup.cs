f0de5847726282be366a70acdf07fda55e4b88dd
Shengquan Ni <Hiseen@github.noreply.com>
2019-09-28 23:26:45 -0700

    migrating changes

--- TexeraOrleansPrototype/webapi-project/Startup.cs
+++ TexeraOrleansPrototype/webapi-project/Startup.cs
@@ -25,6 +25,18 @@ namespace webapi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc(option => option.EnableEndpointRouting = false).AddNewtonsoftJson();
+
+            // kestrel
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+
+            // IIS
+            services.Configure<IISServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
