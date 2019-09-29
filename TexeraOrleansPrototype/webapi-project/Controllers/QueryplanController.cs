0369eab9ed076b4c92fd20f77160ef2fb451f088
Shengquan Ni <Hiseen@github.noreply.com>
2019-09-28 23:50:18 -0700

    Update QueryplanController.cs

--- TexeraOrleansPrototype/webapi-project/Controllers/QueryplanController.cs
+++ TexeraOrleansPrototype/webapi-project/Controllers/QueryplanController.cs
@@ -34,7 +34,7 @@ namespace webapi.Controllers
             //apply TPC-H Query-1
             //json="{\"logicalPlan\":{\"operators\":[{\"tableName\":\"D:\\\\median_input.csv\",\"operatorID\":\"operator-348819d9-d8f7-4751-b9fb-60d84354c667\",\"operatorType\":\"ScanSource\"},{\"attributeName\":\"_c0\",\"attributeType\":\"string\",\"operatorID\":\"operator-20c3d1f3-bad1-4118-931a-4e28d95eaab1\",\"operatorType\":\"InsertionSort\"},{\"projectionAttributes\":\"_c0\",\"operatorID\":\"operator-9925727b-5a6c-4ce7-884d-34f9368bdfa8\",\"operatorType\":\"Projection\"}],\"links\":[{\"origin\":\"operator-348819d9-d8f7-4751-b9fb-60d84354c667\",\"destination\":\"operator-20c3d1f3-bad1-4118-931a-4e28d95eaab1\"},{\"origin\":\"operator-20c3d1f3-bad1-4118-931a-4e28d95eaab1\",\"destination\":\"operator-9925727b-5a6c-4ce7-884d-34f9368bdfa8\"}]},\"workflowID\":\"texera-workflow-d60c3150-bc78-4304-bf36-757aa7324ef1\"}";
             Console.WriteLine("JSON BODY = " + logicalPlanJson);
-            JObject o = logicalPlanJson;
+            JObject o = (JObject)logicalPlanJson;
             Guid workflowID;
             //remove "texera-workflow-" at the begining of workflowID to make it parsable to Guid
             if(!Guid.TryParse(o["workflowID"].ToString().Substring(16),out workflowID))
