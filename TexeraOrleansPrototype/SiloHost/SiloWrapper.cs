0902542cd60268b9fecae7916595e58dd7e44b5b
Shengquan Ni <Hiseen@github.noreply.com>
2019-11-13 16:29:54 -0800

    increase delay

--- TexeraOrleansPrototype/SiloHost/SiloWrapper.cs
+++ TexeraOrleansPrototype/SiloHost/SiloWrapper.cs
@@ -72,7 +72,7 @@ namespace SiloHost
                 })
                 .ConfigureEndpoints(siloPort: 11111, gatewayPort: 30000)
                 .ConfigureLogging(logging => logging.SetMinimumLevel(LogLevel.Critical).AddConsole())
-                .Configure<SiloMessagingOptions>(options => { options.ResponseTimeout = new TimeSpan(0,4,0); });
+                .Configure<SiloMessagingOptions>(options => { options.ResponseTimeout = new TimeSpan(1,0,0); });
 
             var host = siloBuilder.Build();
             await host.StartAsync();
