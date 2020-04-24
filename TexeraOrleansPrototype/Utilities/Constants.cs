3152848b22ae8f6fa48fc964e2166033e3ce2f67
Shengquan Ni <Hiseen@github.noreply.com>
2020-04-23 23:54:14 -0700

    Update Constants.cs

--- TexeraOrleansPrototype/Utilities/Constants.cs
+++ TexeraOrleansPrototype/Utilities/Constants.cs
@@ -10,16 +10,16 @@ namespace TexeraUtilities
 {
     static public class Constants
     {
-        public static string ClientIPAddress="10.128.0.2";
-        public static string WebHDFSEntry = "http://10.138.0.2:9870/webhdfs/v1/";
+        public static string ClientIPAddress="localhost";
+        public static string WebHDFSEntry = "http://localhost:9870/webhdfs/v1/";
         public static int MaxRetries = 60;
         public static int BatchSize = 400;
-        public volatile static int DefaultNumGrainsInOneLayer=0;
+        public volatile static int DefaultNumGrainsInOneLayer=1;
         public static string ConnectionString 
         {
             get
             {
-                return "server="+ClientIPAddress+";uid=orleans-backend;pwd=orleans-0519-2019;database=orleans;SslMode=none";
+                return "server="+ClientIPAddress+";uid=orleansbackend;pwd=orleans-0519-2019;database=orleans;SslMode=none";
             }
         }
     }
