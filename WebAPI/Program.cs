1b017ba392db8fb96d7a5a902362642eda67d21c
Avinash Kumar <avinash0161@yahoo.com>
2020-06-03 20:32:48 -0700

    Added a console program to run the workflow

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 using Orleans;
 using OrleansClient;
 using TexeraUtilities;
@@ -50,9 +51,49 @@ namespace webapi
             {
                 Constants.MaxRetries=MaxRetries;
             }
-            Console.WriteLine("Ready to build connection...");
+            Console.WriteLine("Ready to execute workflow :-) ");
             client=ClientWrapper.Instance.client;
-            BuildWebHost().Run();
+
+
+            // BuildWebHost().Run();
+            Console.WriteLine("Choose the workflow you would like to run - 1.TPCH-1, 2.TPCH-13");
+            Console.WriteLine("Enter your option: (1 or 2)");
+            
+            String tpch1Workflow = "{\"logicalPlan\":{\"operators\":[{\"tableName\":\"{0}\",\"operatorID\":\"operator-348819d9-d8f7-4751-b9fb-60d84354c667\",\"operatorType\":\"ScanSource\"},{\"attributeName\":\"_c0\",\"attributeType\":\"string\",\"operatorID\":\"operator-20c3d1f3-bad1-4118-931a-4e28d95eaab1\",\"operatorType\":\"InsertionSort\"},{\"projectionAttributes\":\"_c0\",\"operatorID\":\"operator-9925727b-5a6c-4ce7-884d-34f9368bdfa8\",\"operatorType\":\"Projection\"}],\"links\":[{\"origin\":\"operator-348819d9-d8f7-4751-b9fb-60d84354c667\",\"destination\":\"operator-20c3d1f3-bad1-4118-931a-4e28d95eaab1\"},{\"origin\":\"operator-20c3d1f3-bad1-4118-931a-4e28d95eaab1\",\"destination\":\"operator-9925727b-5a6c-4ce7-884d-34f9368bdfa8\"}]},\"workflowID\":\"texera-workflow-d60c3150-bc78-4304-bf36-757aa7324ef1\"}";
+            String tpch13Workflow = "";
+
+            String inputWorkflow = Console.ReadLine();
+            String workflowChosen = inputWorkflow.Contains("1")?tpch1Workflow:tpch13Workflow;
+            if(inputWorkflow.Contains("1"))
+            {
+                Console.WriteLine("Where is your input data file? eg: D:\\\\input.txt");
+                String file1 = Console.ReadLine();
+                workflowChosen = workflowChosen.Replace("{0}",file1);
+            } else {
+                // implement for tpch-13
+            }
+
+            JObject o = JObject.Parse(workflowChosen);
+            Guid workflowID;
+            //remove "texera-workflow-" at the begining of workflowID to make it parsable to Guid
+            if(!Guid.TryParse(o["workflowID"].ToString().Substring(16),out workflowID))
+            {
+                throw new Exception($"Parse workflowID failed! For {o["workflowID"].ToString().Substring(16)}");
+            }
+            List<TexeraTuple> results = ClientWrapper.Instance.DoClientWork(client, workflowID, o["logicalPlan"].ToString()).Result;
+            if(results == null)
+            {
+                results = new List<TexeraTuple>();
+            }
+            
+            Console.WriteLine("RESULTS:");
+            results.ForEach(tuple=> {
+                Console.WriteLine();
+                for(int i=0; i<tuple.FieldList.Length; i++) {
+                    Console.Write(tuple.FieldList[i]+",");
+                }
+                Console.WriteLine();
+            });
         }
 
         public static IWebHost BuildWebHost() =>
